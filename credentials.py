f30ea1f576b68a2e75c948b15609217f24353fd6
posilash <114579277+posilash@users.noreply.github.com>
2022-09-28 12:36:43 +0100

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,11 @@
+# This is just a sample file. You need to
+# edit this file. You need to get these
+# details from your Twitter app settings.
+
+bearer_token = ''
+consumer_key = ''
+consumer_secret = ''
+client_id = ''
+client_secret = ''
+access_token = ''
+access_token_secret = ''
